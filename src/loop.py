1d435070617e498be4cb2bd106d7706a09bc3b9f
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-09-26 12:34:14 -0300

    retorna interface com referee e organiza codigo

--- src/loop.py
+++ src/loop.py
@@ -4,7 +4,9 @@ from strategy import MainStrategy
 from UVF_screen import UVFScreen
 from communication.serialRadio import SerialRadio
 from world import World
-from client import VSS
+
+# Importa interface com FiraSim
+# from client import VSS
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -26,12 +28,12 @@ class Loop:
         port=5001,
         n_robots=3,
     ):
-        # Instancia interface com o simulador
+        # Instancia interface com o simulador FIRASim
         #self.vss = VSS(team_yellow=team_yellow)
 
         # Instancia interfaces com o referee
-        #self.rc = RefereeCommands()
-        #self.rp = RefereePlacement(team_yellow=team_yellow)
+        self.rc = RefereeCommands()
+        self.rp = RefereePlacement(team_yellow=team_yellow)
 
         # Instancia o mundo e a estratégia
         self.world = World(n_robots=n_robots, side=team_side, team_yellow=team_yellow, immediate_start=immediate_start)
@@ -53,32 +55,25 @@ class Loop:
             self.UVF_screen.initialiazeScreen()
             self.UVF_screen.initialiazeObjects()
         
-                   
-        
-
     def loop(self):
         if self.world.updateCount == self.lastupdatecount: return
-        
-        # print("loop ALP:",(time.time()-self.t0)*1000)
-
-        self.t0 = time.time()
-        
         self.lastupdatecount = self.world.updateCount
-
+        
+        # Para verificar o tempo de loop:
+        # print("loop ALP:",(time.time()-self.t0)*1000)
+        # self.t0 = time.time()
+        
         # Executa estratégia
         self.strategy.update()
 
         # Executa o controle
         control_output = [robot.entity.control.actuate(robot) for robot in self.world.team if robot.entity is not None]
-
         if self.execute:
             for robot in self.world.raw_team: robot.turnOn()   
             self.radio.send(control_output)
-            self.busyLoop()
         else:
             self.radio.send([(0,0) for robot in self.world.team])
             for robot in self.world.raw_team: robot.turnOff()
-            self.busyLoop()
 
         # Desenha no ALP-GUI
         self.draw()
@@ -88,13 +83,14 @@ class Loop:
         self.execute = True if message else False
         if self.execute: self.world.update(message.detection)
         
-        # command = self.rc.receive()
-        # if command is not None: self.strategy.manageReferee(self.rp, command)
+        command = self.rc.receive()
+        if command is not None: self.strategy.manageReferee(self.rp, command)
 
     def draw(self):
         for robot in [r for r in self.world.team if r.entity is not None]:
             clientProvider().drawRobot(robot.id, robot.x, robot.y, robot.thvec_raw.vec[0], robot.direction)
 
+        # Plota inimigos no ALP-GUI
         # for robot in self.world.enemies:
         #     clientProvider().drawRobot(robot.id+3, robot.x, robot.y, robot.thvec_raw.vec[0], 1, (0.6, 0.6, 0.6))
 
@@ -106,12 +102,10 @@ class Loop:
         logging.info("System is running")
         
         while self.running:
-            # Executa o loop de visão e referee até dar o tempo de executar o resto
-            # self.busyLoop()
-            # while time.time() - t0 < self.loopTime:
-            #     self.busyLoop()
 
-            if(self.world.updateCount == 0):
+            # Executa o loop de visão e referee até dar o tempo de executar o resto
+            self.busyLoop()
+            while time.time() - t0 < self.loopTime:
                 self.busyLoop()
                 
             # Tempo inicial do loop

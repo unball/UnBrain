be46a56d77dac2f0fa96bd74e321d688558e1ad1
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-09-29 19:59:55 -0300

    implementa rotina para envio de v e w

--- src/strategy/automaticReplacer.py
+++ src/strategy/automaticReplacer.py
@@ -1,7 +1,7 @@
 
 from client.referee import RefereeCommands
 from strategy.entity.automaticPlacement import AutomaticPlacement
-
+import numpy as np
 class AutomaticReplacer():
     def __init__(self, automaticPositions):
         super().__init__(automaticPositions)
@@ -9,22 +9,21 @@ class AutomaticReplacer():
         self.pose = automaticPositions
 
     def send(self):
-
+        rr = np.array(self.robot.pos)
         ap = AutomaticPlacement()
 
         # atualiza entidade de todos os robos para posicionamento automatico
-        self.world.team[2].updateEntity(ap(self.pose[0]))
+        self.world.team[0].updateEntity(ap(self.pose[0]))
         self.world.team[1].updateEntity(ap(self.pose[1]))
-        self.world.team[0].updateEntity(ap(self.pose[2]))
+        self.world.team[2].updateEntity(ap(self.pose[2]))
 
         # para todos os robos quando receber foul que nao seja game on, stop ou halt
         # envia v, w = 0 para robos 0,1,2 
         control_output = [(0,0), (0,0), (0,0)] 
         self.radio.send(control_output)
 
-        # enquanto nao chegar na posição desejada, calcula v,w e envia para robos
-        # while (nao chegou na posição desejada)
-        control_output = [robot.entity.control.actuate(robot) for robot in self.world.team if robot.entity is not None] 
-        self.radio.send(control_output)
+        while(rr[0] > self.pose[0] or rr[1] > self.pose[1] or rr[2] > self.pose[2] ):
+            control_output = [robot.entity.control.actuate(robot) for robot in self.world.team if robot.entity is not None] 
+            self.radio.send(control_output)
         
-        # quando chegar, corrige orientação dos robos
+        

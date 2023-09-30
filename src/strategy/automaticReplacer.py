e0587a6237ad32eddef373f92bd44e4e9f81fc2b
Rodrigo <221029051@aluno.unb.br>
2023-09-30 18:46:04 -0300

    corrige automatic replacer com estrutura basica de codigo

--- src/strategy/automaticReplacer.py
+++ src/strategy/automaticReplacer.py
@@ -1,21 +1,37 @@
 
 from client.referee import RefereeCommands
 from strategy.entity.automaticPlacement import AutomaticPlacement
-import numpy as np
+import numpy as np 
+from communication.serialWifi import SerialRadio
+
 class AutomaticReplacer():
-    def __init__(self, automaticPositions=[]):
+    def __init__(self, world):
         super().__init__()
 
-        self.pose = automaticPositions
+        self.world = world
+        self.radio = SerialRadio()
 
-    def send(self):
-        rr = np.array(self.robot.pos)
-        ap = AutomaticPlacement()
+    def send(self, pose):
+        #rr = np.array(self.robot.pose)
+        # ap = AutomaticPlacement()
+
+        rr0 = np.array(self.world.team[pose[0][0]].pose)
+        rr1 = np.array(self.world.team[pose[1][0]].pose)
+        rr2 = np.array(self.world.team[pose[2][0]].pose)
 
         # atualiza entidade de todos os robos para posicionamento automatico
-        self.world.team[0].updateEntity(ap(self.pose[0]))
-        self.world.team[1].updateEntity(ap(self.pose[1]))
-        self.world.team[2].updateEntity(ap(self.pose[2]))
+        
+        # ap = AutomaticPlacement(self.world, self.world.team[pose[0][0]], pose[0][1])
+        # self.world.team[pose[0][0]].updateEntity(AutomaticPlacement)
+        # ap = AutomaticPlacement(self.world, self.world.team[pose[1][0]],pose[1][1])
+        # self.world.team[pose[1][0]].updateEntity(AutomaticPlacement)
+        # ap = AutomaticPlacement(self.world, self.world.team[pose[2][0]],pose[2][1])
+        # self.world.team[pose[2][0]].updateEntity(AutomaticPlacement)
+
+        # TODO: descobrir quais saos os ids dos robos de verdade durante o jogo
+        self.world.team[pose[0][0]].entity = AutomaticPlacement(self.world, self.world.team[pose[0][0]], pose[0][1])
+        self.world.team[pose[1][0]].entity = AutomaticPlacement(self.world, self.world.team[pose[1][0]], pose[1][1])
+        self.world.team[pose[2][0]].entity = AutomaticPlacement(self.world, self.world.team[pose[2][0]], pose[2][1])
 
         for robot in self.world.team:
             robot.updateSpin()
@@ -28,7 +44,16 @@ class AutomaticReplacer():
         control_output = [(0,0), (0,0), (0,0)] 
         self.radio.send(control_output)
 
-        while(rr[0] > self.pose[0] or rr[1] > self.pose[1] or rr[2] > self.pose[2] ):
+
+        while(rr0[0] > pose[0][1][0] or rr0[1] > pose[0][1][1] or rr0[2] > pose[0][1][2] ):
+            control_output = [robot.entity.control.actuate(robot) for robot in self.world.team if robot.entity is not None] 
+            self.radio.send(control_output)
+
+        while(rr1[0] > pose[1][1][0] or rr1[1] > pose[1][1][1] or rr1[2] > pose[1][1][2] ):
+            control_output = [robot.entity.control.actuate(robot) for robot in self.world.team if robot.entity is not None] 
+            self.radio.send(control_output)
+
+        while(rr2[0] > pose[2][1][0] or rr2[1] > pose[2][1][1] or rr2[2] > pose[2][1][2] ):
             control_output = [robot.entity.control.actuate(robot) for robot in self.world.team if robot.entity is not None] 
             self.radio.send(control_output)
         

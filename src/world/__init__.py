e00db9f4071170f9777553d1cf1c109ac0b3fa7f
raulmyron <raul.myron@gmail.com>
2023-10-08 20:01:05 -0300

    adding flags

--- src/world/__init__.py
+++ src/world/__init__.py
@@ -32,7 +32,7 @@ class Field:
     @property
     def marginX(self):
         return self.maxX - self.xmargin
-    
+
     @property
     def marginY(self):
         return self.maxY - self.ymargin
@@ -49,11 +49,13 @@ class Field:
     def goalAreaSize(self):
         return (self.goalAreaWidth, self.goalAreaHeight)
 
+
 class World:
-    def __init__(self, n_robots=3, side=1, vss=None, team_yellow=False, immediate_start=False, control=False):
+    def __init__(self, n_robots=3, side=1, vss=None, team_yellow=False, immediate_start=False, control=False, debug=False, referee=False):
         self.n_robots = n_robots
-        self._team = [TeamRobot(self, i, on=immediate_start) for i in range(self.n_robots)]
-        #self.enemies = [TeamRobot(self, i, on=immediate_start) for i in range(self.n_robots)]
+        self._team = [TeamRobot(self, i, on=immediate_start)
+                      for i in range(self.n_robots)]
+        # self.enemies = [TeamRobot(self, i, on=immediate_start) for i in range(self.n_robots)]
         self.ball = Ball(self)
         self.field = Field(side)
         self.vss = vss
@@ -65,61 +67,75 @@ class World:
         self.manualControlSpeedV = 0
         self.manualControlSpeedW = 0
         self.control = control
-        
+        self.referee = referee
+        self.debug = debug
+
     def update(self, message):
-        if self.team_yellow: 
+        if self.team_yellow:
             yellow = self.team
-            #blue = self.enemies
+            # blue = self.enemies
         else:
-            #yellow = self.enemies
+            # yellow = self.enemies
             blue = self.team
 
         robot_id = 0
+        
+        #print(message)
+        
 
         if self.team_yellow:
-            print("yellow")
+            
             team = message.robots_yellow
             for _ in team:
                 # Pela vsss vision recebemos em mm e nossa estrategia usa m
+                if self.debug:
+
+                    print(f"Yellow - {robot_id}")
+                    
+                    print(f"x {(message.robots_yellow[robot_id].x) / 1000:.2f} | y {(message.robots_yellow[robot_id].y) / 1000:.2f} | th {(message.robots_yellow[robot_id].orientation):.2f}")
+                    
                 yellow[robot_id].update(
                     message.robots_yellow[robot_id].x / 1000,
                     message.robots_yellow[robot_id].y / 1000,
-                    message.robots_yellow[robot_id].orientation 
+                    message.robots_yellow[robot_id].orientation
                 )
-                robot_id+=1
+                robot_id += 1
         else:
-            print("blue")
+            # print("blue")
             team = message.robots_blue
-            for item in team:
+    
+            for _ in team:
                 # Pela vsss vision recebemos em mm e nossa estrategia usa m
-                
-                #print(message)
-                print("x",(message.robots_blue[robot_id].x) / 1000)
-                print("y",(message.robots_blue[robot_id].y) / 1000)
-                print("th",(message.robots_blue[robot_id].orientation*-1))
-                print("bola x",message.balls[0].x  / 1000)
-                print("bola y",message.balls[0].y / 1000)
-                #exit()
-                
-                print(robot_id)
-                
-                blue[robot_id].update(
-                    (message.robots_blue[robot_id].x / 1000 ),
-                    (message.robots_blue[robot_id].y / 1000) ,
-                    (message.robots_blue[robot_id].orientation) * 1
+
+                if self.debug:
+                    print("Blue")
+                    print(f"x {((message.robots_blue[robot_id].x) / 1000):.2f} y {(message.robots_blue[robot_id].y) / 1000:.2f} th {(message.robots_blue[robot_id].orientation):.2f}")
+
+                    """ with open('docs/robot.txt', 'a') as f:
                     
+                            f.write(str(math.floor(message.robots_blue[0].x))+" "+str(math.floor(message.robots_blue[0].y))+" " + str((message.robots_blue[0].orientation))+(' \n'))"""
+
+                blue[robot_id].update(
+                    (message.robots_blue[robot_id].x / 1000),
+                    (message.robots_blue[robot_id].y / 1000),
+                    (message.robots_blue[robot_id].orientation),
+
                 )
-                with open('docs/robot.txt', 'a') as f:
-                
-                    f.write(str(math.floor(message.robots_blue[0].x))+" "+str(math.floor(message.robots_blue[0].y))+" " + str((message.robots_blue[0].orientation))+(' \n'))
-                
-                robot_id+=1
-        self.ball.update((message.balls[0].x) / 1000 , (message.balls[0].y)/ 1000 )
+
+
+        if self.team_yellow:
+            self.ball.update((message.balls[0].x) /1000, (message.balls[0].y) / 1000)
+        else:
+            self.ball.update((message.balls[0].x) /1000, (message.balls[0].y) / 1000)
+
+        if self.debug:
+            print(f"bola {(message.balls[0].x/1000):.2f} {(message.balls[0].y / 1000):.2f}")
+
         # self.checkBatteries = message["check_batteries"]
         # self.manualControlSpeedV = message["manualControlSpeedV"]
         # self.manualControlSpeedW = message["manualControlSpeedW"]
-        #logging.info("Vision update.")
-            
+        # logging.info("Vision update.")
+
         self.updateCount += 1
 
     def addAllyGoal(self):
@@ -140,7 +156,7 @@ class World:
 
     @property
     def team(self):
-        return self._team#[robot for robot in self._team if robot.on]
+        return self._team  # [robot for robot in self._team if robot.on]
 
     @property
     def raw_team(self):

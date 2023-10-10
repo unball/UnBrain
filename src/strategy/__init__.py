ef54b2c35d68df01fff30baeabf721a914582306
raulmyron <raul.myron@gmail.com>
2023-10-10 17:47:58 -0300

    make referee start stop working

--- src/strategy/__init__.py
+++ src/strategy/__init__.py
@@ -37,23 +37,23 @@ class MainStrategy(Strategy):
         # Variables
         self.static_entities = static_entities
 
-    def manageReferee(self, arp, command):
+    def manageReferee(self, arp, command, initiated_once):
 
         # Pegar apenas id que existe dos robos
         robot_id = []
         for robot in self.world.team:
             robot_id.append(robot.id)
 
-        if command is None: return
+        if command is None: 
+            
+            if(not initiated_once):
+                
+                for robot in self.world.raw_team: 
+                    robot.turnOff()
+            return
         
         self.goalkeeperIndx = None
         self.AttackerIdx = None
-        
-        if self.world.debug and command is not None:
-            print("\n\n\n")
-            print(command.gameHalf)
-            print("\n\n\n------------------")
-            #exit()
             
         # Verifica gol
         if command.foul == Foul.KICKOFF:
@@ -97,9 +97,8 @@ class MainStrategy(Strategy):
                 print("COMANDO STOP OU HALT ENVIADO")
             
             for robot in self.world.raw_team: 
-                #self.radio.send([(0,0) for robot in self.world.team])
-                #robot.turnOff()
-                pass
+                robot.turnOff()
+
     
     def nearestGoal(self, indexes):
         rg = np.array([-0.75, 0])

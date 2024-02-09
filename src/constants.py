4eb1b986d0a38db0227ad48a84082a4db9dc3cbf
maranhas02 <221029051@aluno.unb.br>
2024-02-09 08:32:01 -0300

    Debug actuate False

--- src/constants.py
+++ src/constants.py
@@ -19,7 +19,7 @@ DEBUG_WIFI = False #Variable that verifies if the sendWifi is working correctly
 SHOW_DEBUG_WIFI_ERROR = False
 if DEBUG_WIFI:
     SHOW_DEBUG_WIFI_ERROR = True  #it disables the default serial default debug. ("Falha ao abrir serial: Causa")
-DEBUG_ACTUATE = True  #Variable that verifies if the actuate is sending the methods (use it for when there's no transmiter),
+DEBUG_ACTUATE = False  #Variable that verifies if the actuate is sending the methods (use it for when there's no transmiter),
 
 HOST_FIRASIM_VISION = "224.0.0.1"
 HOST_FIRASIM_COMMAND = "127.0.0.1"

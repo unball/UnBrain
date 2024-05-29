ab192424d7f90c923daeb8cab7315acc65a20543
MariaClaudia1328 <mariaclaudia8.0@hotmail.com>
2024-05-29 14:30:33 -0300

    altera valor de mu para borracha dura com madeira seca

--- src/control/UFC.py
+++ src/control/UFC.py
@@ -7,7 +7,7 @@ import time
 
 class UFC_Simple(Control):
     """Controle unificado para o Univector Field, utiliza o ângulo definido pelo campo como referência \\(\\theta_d\\)."""
-    def __init__(self, world, kw=4, kp=10, mu=0.7, vmax=0.7, L=L, enableInjection=False):
+    def __init__(self, world, kw=4, kp=10, mu=0.95, vmax=0.7, L=L, enableInjection=False):
       Control.__init__(self, world)
 
       self.g = 9.8

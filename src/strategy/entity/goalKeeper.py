f6aa615ab6997787775db2a04370e5a769981a99
maranhas02 <221029051@aluno.unb.br>
2024-06-03 16:12:48 -0300

    Merge branch 'origin/Goalkeeper_novo'

--- src/strategy/entity/goalKeeper.py
+++ src/strategy/entity/goalKeeper.py
@@ -1,5 +1,6 @@
 from ..entity import Entity
 from strategy.field.UVF import UVF, UVFDefault
+from strategy.field.UVF import UVF, UVFDefault
 from strategy.field.DirectionalField import DirectionalField
 from strategy.field.goalKeeper import GoalKeeperField
 from strategy.field.attractive import AttractiveField
@@ -56,75 +57,24 @@ class GoalKeeper(Entity):
                     self.robot.direction *= -1
 
     def fieldDecider(self):
-        rr = np.array(self.robot.pos)
-        vr = np.array(self.robot.v)
-        # self.vravg = 0.995 * self.vravg + 0.005 * norml(vr)
-        rb = np.array(self.world.ball.pos)
-        vb = np.array(self.world.ball.v)
+        rr = np.array(self.robot.pose)
+        rb = np.array(self.world.ball.pos.copy())
+        vb = np.array(self.world.ball.v.copy())
         rg = -np.array(self.world.field.goalPos)
+        # Dados necessários para a decisão
+        rg = -np.array(self.world.field.goalPos)
+        rg[0] += 0.03
+        self.setAttackerControl()
 
-    
-         # Aplica o movimento
+        
         self.robot.gammavels = (0,0,0)
         self.robot.vref = 0
 
-        self.robot.setSpin(spinGoalKeeper(rb, rr, rg), timeOut = 0.13)
-
-        Pb = goalkeep(rb, vb, rr, rg)
-
+        self.robot.setSpin(spinGoalKeeper(rb, rr, rg), timeOut = 0.1)
+        
         if np.abs(rr[0]-rg[0]) > 0.12:
             pose = goalkeep(rb, vb, rr, rg)
             self.robot.field = UVFDefault(self.world, pose, rr, direction=0, spiral=False)
         else: 
             pose = goalkeep(rb, vb, rr, (rr[0], rg[1]))
             self.robot.field = GoalKeeperField(pose, rg[0])
-        #self.robot.field = UVFDefault(self.world, (rr[0], *pose[1:3]), rr, direction=0, spiral=False)
-        #else: self.robot.field = GoalKeeperField((rr[0], *pose[1:3]))
-        #self.robot.field = UVFDefault(self.world, pose, direction=0, radius=0.14)
-
-        # if self.state == "Stable":
-        #     """if self.robot.field is not None:
-        #         ref_th = self.robot.field.F(self.robot.pose)
-        #         rob_th = self.robot.th
-        #         if abs(angError(ref_th, rob_th)) > 45 * np.pi / 180:
-        #             print('unstable ANG')
-        #             self.robot.setSpin(1, timeOut = 0.13)"""
-        #     if np.abs(rr[0]-rg[0]) > 0.03: #or abs(angError(self.robot.th, ang(rr, rg))) > 40 * np.pi / 180:
-        #         print(np.abs(rr[0]-rg[0]) > 0.03)
-        #         print('unstable')
-        #         self.state = "Unstable"
-        #         """if self.robot.field is not None:
-        #             ref_th = self.robot.field.F(self.robot.pose)
-        #             rob_th = self.robot.th
-        #             if abs(angError(ref_th, rob_th)) > 45 * np.pi / 180:
-        #                 print('unstable ANG')
-        #                 self.robot.setSpin(1, timeOut = 0.05)"""
-        # elif self.state == "Unstable":
-        #     if rr[0] > 0:
-        #         print("Attacker Control acessado")
-        #         self.setAttackerControl()
-        #         self.state = "Far"
-        #     elif np.abs(rr[0]-rg[0]) < 0.015:
-        #         self.state = "Stable"
-        # else:
-        #     print('else')
-        #     if np.abs(rr[0]-rg[0]) < 0.015:
-        #         print("IF")
-        #         self.state = "Stable"
-        #         self.setGoalKeeperControl()
-
-        # #self.robot.field = UVF(Pb, spiral=0.01)
-        # #self.robot.field = DirectionalField(Pb[2], Pb=Pb) if np.abs(rr[0]-Pb[0]) < 0.07 else UVF(Pb, spiral=0.01)
-
-        # if self.state == "Stable":
-        #     print("entrou no stable")
-        #     self.robot.field = DirectionalField(Pb[2], Pb=(rr[0], Pb[1], Pb[2]))
-        # elif self.state == "Unstable":
-        #     print("entrou no unstable")
-        #     #self.robot.field = UVF(Pb, radius=0.02)
-        #     #self.robot.field = GoalKeeperField(Pb, rg[0]-0.02)
-        #     self.robot.field = AttractiveField((rg[0]-0.02, Pb[1], Pb[2]))
-        # elif self.state == "Far":
-        #     print("entrou no far")
-        #     self.robot.field = UVF(Pb, radius=0.04)
-        # #self.robot.field = DirectionalField(Pb[2], Pb=Pb)
--- src/strategy/field/goalKeeper.py
+++ src/strategy/field/goalKeeper.py
@@ -5,13 +5,11 @@ from . import Field
 class GoalKeeperField(Field):
   def __init__(self, Pb, xgoal):
     super().__init__(Pb)
-    self.Pb = Pb
     self.y = Pb[1]
     self.x = xgoal
     self.A = (10*np.pi/180) / 0.08
 
   def F(self, P, Pb=None, retnparray=False):
-    P = np.array(P)
     if len(P.shape) == 1: P = np.array([P]).T
 
     c0 = np.abs(P[0]-self.Pb[0]) > 0.07
@@ -19,8 +17,9 @@ class GoalKeeperField(Field):
     c2 = np.bitwise_not(c1)
 
     uvf = np.zeros_like(P[0])
-    uvf[c1] = -np.pi/2 + np.arctan(-(deadZoneDisc(P[0][c1] - self.x, 0.08)) * self.A)
-    uvf[c2] = np.pi/2 + np.arctan((deadZoneDisc(P[0][c2] - self.x, 0.08)) * self.A)
+    uvf[c1] = -np.pi/2 - (P[0][c1] - self.x) * self.A
+    uvf[c2] = np.pi/2 + (P[0][c2] - self.x) * self.A
+    #uvf = ang(P, self.Pb)
 
     if uvf.size == 1 and not(retnparray): return uvf[0]
     return uvf

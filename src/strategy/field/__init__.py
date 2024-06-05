eb05d14f8b07f0c156e21090b33ee78cb4ca5958
maranhas02 <221029051@aluno.unb.br>
2024-06-05 15:55:08 -0300

    Voltando Gamma e Phi

--- src/strategy/field/__init__.py
+++ src/strategy/field/__init__.py
@@ -18,14 +18,9 @@ class Field(ABC):
     def phi(self, P: tuple, d=0.0001):
         """Calcula o ângulo \\(\\phi = \\frac{\\partial \\theta_d}{\\partial x} \\cos(\\theta) + \\frac{\\partial \\theta_d}{\\partial y} \\sin(\\theta)\\) usado para o controle"""
         P = np.array(P)
-        dx = filt((self.F(P+[d,0,0])-self.F(P-[d,0,0]))/(2*d), 100)
-        dy = filt((self.F(P+[0,d,0])-self.F(P-[0,d,0]))/(2*d), 100)
-        return (dx*np.cos(P[2]) + dy*np.sin(P[2])) / 2.2
+        dx = filt((self.F(P+[d,0,0])-self.F(P-[d,0,0]))/(2*d), 15)
+        dy = filt((self.F(P+[0,d,0])-self.F(P-[0,d,0]))/(2*d), 15)
+        return (dx*np.cos(P[2]) + dy*np.sin(P[2]))
 
-    def gamma(self, P: tuple, v: tuple, d=0.00001):
-        P = np.array(P)
-        Pb = np.array(self.Pb)
-        dx = filt((self.F(P, Pb=Pb+[d,0,0])-self.F(P))/d * v[0], 100)
-        dy = filt((self.F(P, Pb=Pb+[0,d,0])-self.F(P))/d * v[1], 100)
-        dth = filt((self.F(P, Pb=Pb+[0,0,d])-self.F(P))/d * v[2], 100)
-        return (dx + dy + dth) / 5
+    def gamma(self, dth, v, phi):
+        return dth - v*phi

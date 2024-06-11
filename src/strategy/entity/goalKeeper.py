c721eac7200451d98042963d11a4f1ac4416d275
maranhas02 <221029051@aluno.unb.br>
2024-06-11 10:47:53 -0300

    ⚙️Consertando estratégia do goleiro

--- src/strategy/entity/goalKeeper.py
+++ src/strategy/entity/goalKeeper.py
@@ -57,24 +57,53 @@ class GoalKeeper(Entity):
                     self.robot.direction *= -1
 
     def fieldDecider(self):
-        rr = np.array(self.robot.pose)
-        rb = np.array(self.world.ball.pos.copy())
-        vb = np.array(self.world.ball.v.copy())
+        rr = np.array(self.robot.pos)
+        thr = self.robot.pose[2]
+        vr = np.array(self.robot.v)
+        # self.vravg = 0.995 * self.vravg + 0.005 * norml(vr)
+        rb = np.array(self.world.ball.pos)
+        vb = np.array(self.world.ball.v)
         rg = -np.array(self.world.field.goalPos)
-        # Dados necessários para a decisão
-        rg = -np.array(self.world.field.goalPos)
-        rg[0] += 0.03
-        self.setAttackerControl()
-
-        
-        self.robot.gammavels = (0,0,0)
+        rg[0] += 0.06
+    
+         # Aplica o movimento
         self.robot.vref = 0
 
         self.robot.setSpin(spinGoalKeeper(rb, rr, rg), timeOut = 0.1)
-        
-        if np.abs(rr[0]-rg[0]) > 0.12:
-            pose = goalkeep(rb, vb, rr, rg)
-            self.robot.field = UVFDefault(self.world, pose, rr, direction=0, spiral=False)
-        else: 
-            pose = goalkeep(rb, vb, rr, (rr[0], rg[1]))
-            self.robot.field = GoalKeeperField(pose, rg[0])
+
+        Pb = goalkeep(rb, vb, rr, rg)
+        # print(Pb)
+        # print('isAlive:' , self.robot.isAlive())
+        # print(f"angulo: {thr}")
+        if self.state == "Stable":
+            # if np.abs(rr[0]-rg[0]) > 0.05 or (np.abs(thr-1.5) > 2 or np.abs(thr-4.7) > 2):
+            if np.abs(rr[0]-rg[0]) > 0.07:
+                self.state = "Unstable"
+                self.setGoalKeeperControl()
+        elif self.state == "Unstable":
+            if rr[0] > 0:
+                self.setAttackerControl()
+                self.state = "Far"
+                # SETAR INSTABILIDADE CASO O ROBO ESTEJA VIRADO E FRENTE PRO GOL
+                
+            # elif (np.abs(rr[0]-rg[0]) < 0.05) and (np.abs(thr-1.5) < 2 or np.abs(thr-4.7) < 2):
+            elif (np.abs(rr[0]-rg[0]) < 0.05):
+                self.state = "Stable"
+                self.setGoalKeeperControl()
+        else:
+            # if (np.abs(rr[0]-rg[0]) < 0.05) and (np.abs(thr-1.5) < 2 or np.abs(thr-4.7) < 2):
+            if (np.abs(rr[0]-rg[0]) < 0.05):
+                self.state = "Stable"
+                self.setGoalKeeperControl()
+
+        # self.robot.field = UVF(Pb, spiral=0.01)
+        # self.robot.field = DirectionalField(Pb[2], Pb=Pb) if np.abs(rr[0]-Pb[0]) < 0.07 else UVF(Pb, spiral=0.01)
+        # print('estado do goleiro: ' + self.state)
+        if self.state == "Stable":
+            self.robot.field = DirectionalField(Pb[2], Pb=(rr[0], Pb[1], Pb[2]))
+        elif self.state == "Unstable":
+            # self.robot.field = UVF(Pb, radius=0.02)
+            self.robot.field = AttractiveField((rg[0], Pb[1], Pb[2]))
+        elif self.state == "Far":
+            self.robot.field = UVFDefault(self.world,  Pb, rr, 0)
+        #self.robot.field = DirectionalField(Pb[2], Pb=Pb)

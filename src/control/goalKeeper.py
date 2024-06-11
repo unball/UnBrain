2d788602a7a0fc1a9d431972855bc5fd24addfd0
maranhas02 <221029051@aluno.unb.br>
2024-06-11 15:00:55 -0300

    ⚙️Consertando o L para o GoalKeeper Control e Defender Control

--- src/control/goalKeeper.py
+++ src/control/goalKeeper.py
@@ -7,7 +7,7 @@ import time
 
 class GoalKeeperControl(Control):
   """Controle unificado para o Univector Field, utiliza o ângulo definido pelo campo como referência \\(\\theta_d\\)."""
-  def __init__(self, world, kw=5, kp=100, mu=0.07, vmax=2, L=0.0605):
+  def __init__(self, world, kw=3, kp=100, mu=0.7, vmax=0.5, L=0.605):
     Control.__init__(self, world)
 
     self.g = 9.8
@@ -31,58 +31,41 @@ class GoalKeeperControl(Control):
 
   def output(self, robot):
     if robot.field is None: return 0,0
-
-    # self.vmax = 2*self.world.manualControlSpeedV
-    # self.kw = 2*self.world.manualControlSpeedW
-
     # Ângulo de referência
     th = robot.field.F(robot.pose)
-
     # Erro angular
     eth = angError(th, robot.th)
-
     # Tempo desde a última atuação
     dt = self.interval.getInterval()
-
+    self.ieth = sat(self.ieth + eth*dt, 5)
     # Derivada da referência
-    dth = sat(angError(th, self.lastth) / dt, 15)
-    
-    # Computa phi
-    phi = robot.field.phi(robot.pose)
+    dth = filt(angError(th, self.lastth) / dt, 100)
 
-    # Computa gamma
-    gamma = robot.field.gamma(robot.pose, robot.gammavels)
-
-    # Computa omega
-    omega = self.kw * np.sign(eth) * np.sqrt(np.abs(eth)) + gamma
+    # Lei de controle da velocidade angular
+    w = dth + self.kw * np.sign(eth) * np.sqrt(np.abs(eth)) #+ self.kw/100.0 * self.ieth 
 
     # Velocidade limite de deslizamento
-    if phi != 0:
-      v1 = (-np.abs(omega) + np.sqrt(omega**2 + 4 * np.abs(phi) * self.amax)) / (2*np.abs(phi))
-    if phi == 0:
-      v1 = self.amax / np.abs(omega)      
+    v1 = self.amax / np.abs(w) if w != 0 else math.inf
 
     # Velocidade limite das rodas
-    v2 = (2*self.vmax - self.L * np.abs(omega)) / (2 + self.L * np.abs(phi))
+    v2 = self.vmax - self.L * np.abs(w) / 2
 
     # Velocidade limite de aproximação
-    v3 = self.kp * norm(robot.pose, robot.field.Pb) ** 2 + robot.vref
+    dTarget = norm(robot.pos, robot.field.Pb) if robot.pos[0] > -.6 else np.abs(robot.field.Pb[1] - robot.pos[1])
+    e = (dTarget-0.015) ** 2
+    self.iep = self.iep + dt*e if self.iep < 1.5 else 0
+
+    v3 = self.kp * e + self.kp/80 * self.iep  + robot.vref if dTarget > 0.015 else 0
 
     # Velocidade linear é menor de todas
     v  = max(min(v1, v2, v3), 0)
-
-    # Lei de controle da velocidade angular
-    w = v * phi + omega
-
-    # Considera resposta lenta
-    #if tau != 0: w = (w - w0 * tau/dt * (1-np.exp(-dt/tau))) / (1-tau/dt * (1-np.exp(-dt/tau)))
-    
-    # Satura w caso ultrapasse a mudança máxima permitida
-    #w  = lastspeed.w + sat(w-lastspeed.w, motorangaccelmax * r * interval / L)
     
     # Atualiza a última referência
     self.lastth = th
     robot.lastControlLinVel = v
 
+    # Atualiza variáveis de estado
+    self.eth = eth
+    
     if robot.spin == 0: return (v * robot.direction, w)
     else: return (0, 60 * robot.spin)

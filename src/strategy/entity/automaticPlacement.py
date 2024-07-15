dd62b41ce66ac04b3353fbcec904700000c985b0
maranhas02 <221029051@aluno.unb.br>
2024-07-15 17:43:58 -0300

    ✨Criação de Automatic Placement parecido com o Control Tester

--- src/strategy/entity/automaticPlacement.py
+++ src/strategy/entity/automaticPlacement.py
@@ -1,5 +1,6 @@
 from ..entity import Entity
 from strategy.field.UVF import UVF
+from tools import angError, howFrontBall, howPerpBall, ang, norml, norm, angl, insideEllipse, angl, unit, projectLine
 from strategy.field.DirectionalField import DirectionalField
 from tools import angError
 from control.UFC import UFC_Simple
@@ -7,21 +8,18 @@ import numpy as np
 import time
 
 class AutomaticPlacement(Entity):
-    def __init__(self, world, robot, automaticPose, side=1):
+    def __init__(self, world, robot, side=1):
         super().__init__(world, robot)
 
         self._control = UFC_Simple(self.world)
         self.lastChat = 0
-        self.goalPose = automaticPose
-        self.spiralRadius = 0.07
-
-        self.outOfPosition = False
+        self.x = 1
 
     @property
     def control(self):
         return self._control
     
-    def equalsTo(self):
+    def equalsTo(self, otherTester):
         return True
 
     def onExit(self):
@@ -32,14 +30,15 @@ class AutomaticPlacement(Entity):
             ref_th = self.robot.field.F(self.robot.pose)
             rob_th = self.robot.th
 
-            if abs(angError(ref_th, rob_th)) > 90 * np.pi / 180:
-                self.robot.direction *= -1
-                self.lastChat = time.time()
+            if time.time()-self.lastChat > 0.5:
+                if abs(angError(ref_th, rob_th)) > 90 * np.pi / 180:
+                    self.robot.direction *= -1
+                    self.lastChat = time.time()
 
             # Inverter a direção se o robô ficar preso em algo
-            elif not self.robot.isAlive() and self.robot.spin == 0:
+            if not self.robot.isAlive() and self.robot.spin == 0:
                 self.lastChat = time.time()
-                self.robot.direction *= -1
+                self.robot.direction *= -1 
 
     def fieldDecider(self):
         rr = np.array(self.robot.pose)
@@ -50,10 +49,23 @@ class AutomaticPlacement(Entity):
         # robo na posição
             # corrige direção com campo direcional
 
-        if( np.abs(self.goalPose[0] - 0.01) <= rr[0] <= np.abs(self.goalPose[0] + 0.01) ) and ( np.abs(self.goalPose[1] - 0.01) <= rr[1] <= np.abs(self.goalPose[1] + 0.01) ):
-            self.robot.field = DirectionalField(self.goalPose[2])
-        else:
-            if np.abs(self.goalPose[1]) > 0:
-                self.robot.field = UVF(self.goalPose, direction=-np.sign(self.goalPose[1]), radius=self.spiralRadius)
-            else: 
-                self.robot.field = UVF(self.goalPose, radius=self.spiralRadius)
+
+        #posições: primeiro = [-0.7, 0.0]; segundo = [-0.5, 0.0]; terceiro = [-0.25, 0.0] 
+        if self.robot.id == 0:
+            if not -0.65 < rr[0] < -0.75 or not -0.05 < rr[1] < 0.05: #Não chegou no lugar certo
+                self.robot.field = DirectionalField(ang(rr,[-0.7, 0.0]), Pb=(-0.7, 0.0 ,2 * np.pi))
+                print(self.robot.id)
+                return
+            while self.x != 1:
+                self.robot.field = DirectionalField((0), Pb=(0.0, 0.0 ,0.0))
+                self.x = 2
+        if self.robot.id == 1:
+            if not -0.45 < rr[0] < -0.55 or not -0.05 < rr[1] < 0.05: #Não chegou no lugar certo
+                self.robot.field = DirectionalField(ang(rr,[-0.5, 0.0]), Pb=(-0.5, 0.0 ,0))
+                print(self.robot.id)
+                return
+        if self.robot.id == 2:
+            if not -0.20 < rr[0] < -0.30 or not -0.05 < rr[1] < 0.05: #Não chegou no lugar certo
+                self.robot.field = DirectionalField(ang(rr,[-0.25, 0.0]), Pb=(-0.25, 0.0 ,2 * np.pi))
+                print(self.robot.id)
+                return

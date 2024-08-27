337ac9cfe0d2b1ba19b9e2c866f5e8e68a788193
Frederico <fredericofabricio5@gmail.com>
2024-08-27 16:31:19 -0300

    refac: refatoramento do update VSSVision e retirado a verificação de pacote do referee

--- src/world/__init__.py
+++ src/world/__init__.py
@@ -275,14 +275,14 @@ class World:
                 if i < len(self.n_robots):
                     #yellow[robot_id].update(message.robots_yellow[robot_id].x,message.robots_yellow[robot_id].y, message.robots_yellow[robot_id].orientation)
                     if self.debug:
-                        print(f"Yellow - {self.n_robots[i]} | x {robot.x} | y {robot.y} | th {robot.orientation} | vx {robot.vx} | vy {robot.vy} | vorientation {robot.vorientation}")
+                        print(f"Yellow - {i} | x {robot.x:.3f} | y {robot.y:.3f} | th {robot.orientation:.3f} | vx {robot.vx:.3f} | vy {robot.vy:.3f} | vorientation {robot.vorientation:.3f}")
                     yellow[self.n_robots[i]].update_FIRASim(robot.x, robot.y, robot.orientation, robot.vx, robot.vy, robot.vorientation)
 
         else:
             for i, robot in enumerate(message.frame.robots_blue):
                 if i < len(self.n_robots):
                     if self.debug:
-                        print(f"Blue - {self.n_robots[i]} | x {robot.x} | y {robot.y} | th {robot.orientation} | vx {robot.vx} | vy {robot.vy} | vorientation {robot.vorientation}")
+                        print(f"Blue - {i} | x {robot.x:.3f} | y {robot.y:.3f} | th {robot.orientation:.3f} | vx {robot.vx:.3f} | vy {robot.vy:.3f} | vorientation {robot.vorientation:.3f}")
                     blue[self.n_robots[i]].update_FIRASim(robot.x, robot.y, robot.orientation, robot.vx, robot.vy, robot.vorientation)
 
         # for robot, pos in zip(self.team, teamPos): robot.update(*pos)

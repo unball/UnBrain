4eb443da7ff178db82b2364e127fe7ee65554bdf
MariaClaudia1328 <mariaclaudia8.0@hotmail.com>
2024-09-13 21:25:22 -0300

    inclui no readme como resolver problema de compilação com protobuf

--- src/client/protobuf/protobuf.sh
+++ src/client/protobuf/protobuf.sh
@@ -1,1 +1,7 @@
+# Usar protoc versão 3.20.1 é obrigatório para compilar os protobufs
+
+# eu baixei essa versão do github, deszipei e botei o caminho
+# completo aqui. Não é gambiarra se funciona!!
+
+#/home/nana/Downloads/protobuf-3.20.1/bin/protoc --python_out=./ *.proto
 protoc --python_out=./ *.proto

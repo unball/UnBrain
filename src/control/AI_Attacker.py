d9198e02aedf2e3741367f99b2823f7f6795751b
fornari03 <221017023@aluno.unb.br>
2025-04-17 22:28:59 -0300

    feat: transforma o atributo model de AI_Control em um atributo da classe

--- src/control/AI_Attacker.py
+++ src/control/AI_Attacker.py
@@ -7,20 +7,22 @@ from ML_algorithms.PPO import PPO
 
 
 class AI_Control(Control):
+        
+    model = None
+
     def __init__(self, world, env, observation):
         Control.__init__(self, world)
-        self.model = None
         self.env = env
         self.observation = observation
         self.time = 0
         self.v_wheel0 = 0
         self.v_wheel1 = 0
+        if AI_Control.model is None:
+            AI_Control.model = PPO(self.env)
+            AI_Control.model.load_model("src/strategy/entity/ppo_models_behind_ball_24")
 
     def output(self, robot):
-        if self.model is None:
-            self.model = PPO(self.env)
-            self.model.load_model("src/strategy/entity/ppo_models_behind_ball_24")
-        actions, _ = self.model.get_action(torch.tensor(self.observation, dtype=torch.float, device=self.model.device))
+        actions, _ = AI_Control.model.get_action(torch.tensor(self.observation, dtype=torch.float, device=AI_Control.model.device))
         if time.time() - self.time > 1/120:
             self.observation = self.env.step(actions.cpu())
             self.time = time.time()

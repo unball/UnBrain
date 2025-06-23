b725f05db7a49fc69a96a690b3b893f5e8aafefa
maranhas2 <rodrigo.maranhao2@gmail.com>
2025-06-23 20:14:11 -0300

    feat: diminui o tempo para testar o isAlive

--- src/world/elements.py
+++ src/world/elements.py
@@ -299,7 +299,7 @@ class TeamRobot(Robot):
         if self.velmod / ctrlVel < 0.1:
             if self.timeLastResponse is not None:
                 dt = time.time() - self.timeLastResponse
-                if dt is not None and dt > 0.33:
+                if dt is not None and dt > 0.15:
                     # self.keepAlive(3)
                     return False
         else:

d09e1b63737a846a9d9b0ec18f1086980c193594
maranhas02 <rodrigo.maranhao2@gmail.com>
2025-08-25 21:56:05 -0300

    Adiciona Testador do magno

--- src/main.py
+++ src/main.py
@@ -2,6 +2,11 @@ from loop import Loop
 import argparse
 import logging
 import client.gui
+import subprocess
+import sys
+import os
+
+import threading
 
 logging.basicConfig(level=logging.INFO)
 
@@ -53,6 +58,11 @@ parser.add_argument('--AI', dest='AI_attacker', action='store_const',
 parser.add_argument('--enemy_AI', dest='enemy_AI', action='store_const',
                     const=True, default=False, help='If you want to use AI_attacker strategy as an enemy.')
 
+parser.add_argument('--systemtest', dest='systemtest',type=str, choices=[
+                                                                        "draw_uvf", # plot do jogo com/sem uvf
+                                                                        "heatmap_team", "heatmap_attacker_ia", "heatmap_attacker", "heatmap_defender", "heatmap_goalkeeper", "heatmap_ball" # heatmaps
+                                                                     ],
+                    default=False, help='Run parallel system instances in test mode.')
 
 args = parser.parse_args()
 
@@ -70,24 +80,82 @@ args.n_robots = [int(e) for e in args.n_robots.split(",")]
 
 print(args.n_robots)
 
-# Instancia o programa principal
-loop = Loop(
-    draw_uvf=False, 
-    team_yellow=team_yellow,
-    immediate_start=args.immediate_start,
-    static_entities=args.static_entities,
-    referee=args.referee,
-    firasim=args.firasim,
-    vssvision=args.vssvision,
-    mainvision=args.mainvision,
-    simulado=args.simulado,
-    control=args.control,
-    debug=args.debug,
-    port=args.port,
-    n_robots=args.n_robots,
-    mirror=mirror, 
-    AI_attacker=args.AI_attacker,
-    enemy_AI = args.enemy_AI
-)
+if args.systemtest:
+    original_argv = sys.argv.copy()
+    if '--systemtest' in original_argv:
+        original_argv.remove('--systemtest')
 
-loop.run()
+    draw_uvf = True if args.systemtest ==  "draw_uvf" else False
+    test_type = args.systemtest if args.systemtest[:7] == "heatmap" else "heatmap_team"
+
+    loop = Loop(
+        draw_uvf=draw_uvf, 
+        team_yellow=team_yellow,
+        immediate_start=args.immediate_start,
+        static_entities=args.static_entities,
+        referee=args.referee,
+        firasim=args.firasim,
+        vssvision=args.vssvision,
+        mainvision=args.mainvision,
+        simulado=True,
+        control=args.control,
+        debug=args.debug,
+        port=args.port,
+        n_robots=args.n_robots,
+        mirror=mirror, 
+        test_type=test_type,
+        AI_attacker=args.AI_attacker,
+        enemy_AI = args.enemy_AI
+    )
+
+    loop.test()
+
+    # loop_list = []
+    # for i in range(10):
+    #     loop = Loop(
+    #     draw_uvf=False, 
+    #     team_yellow=team_yellow,
+    #     immediate_start=args.immediate_start,
+    #     static_entities=args.static_entities,
+    #     referee=args.referee,
+    #     firasim=args.firasim,
+    #     vssvision=args.vssvision,
+    #     mainvision=args.mainvision,
+    #     simulado=True,
+    #     control=args.control,
+    #     debug=args.debug,
+    #     port=args.port,
+    #     n_robots=args.n_robots,
+    #     mirror=mirror)
+
+    #     print(f"tesbols{i}")
+    #     loop_thread = threading.Thread(target=loop.run)
+    #     loop_list.append(loop_thread)
+    #     loop_thread.start() # inicia essa thread do loop
+    
+    # print(threading.active_count())
+    # print(len(loop_list))
+    # loop_thread.join()
+
+else:
+    # Instancia o programa principal
+    loop = Loop(
+        draw_uvf=True, 
+        team_yellow=team_yellow,
+        immediate_start=args.immediate_start,
+        static_entities=args.static_entities,
+        referee=args.referee,
+        firasim=args.firasim,
+        vssvision=args.vssvision,
+        mainvision=args.mainvision,
+        simulado=args.simulado,
+        control=args.control,
+        debug=args.debug,
+        port=args.port,
+        n_robots=args.n_robots,
+        mirror=mirror, 
+        AI_attacker=args.AI_attacker,
+        enemy_AI = args.enemy_AI
+    )
+
+    loop.run()

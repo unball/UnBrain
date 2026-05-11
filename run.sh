ae4d05eb2b9ce8c3a44ba7986ebba950d1cd3036
maranhas02 <rodrigo.maranhao2@gmail.com>
2026-05-11 10:21:58 -0300

    Run corrigido

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ if [[ -n $1 ]]; then
     PORTA="$1"
 fi
 
-( (cd MainVision && ./main.py --port "$PORTA" --n_robots 1) & (sleep 0.5s && python3 src/main.py --team-color blue --mainvision --port "$PORTA" --n_robots 0 --static-entities) )
+( (cd MainVision && ./main.py --port "$PORTA") & (sleep 0.5s && python3 src/main.py --team-color yellow --mainvision --port "$PORTA") )
